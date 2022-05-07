0481ddb3047a293df3ffacfb50814902036a42d0
Takács István Olivér <h052630@stud.u-szeged.hu>
2022-05-07 21:45:13 +0200

    táblázat

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -11,14 +11,35 @@ import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angula
 export class MainComponent implements OnInit {
 
   userForm: FormGroup;
+  listData: any;
 
   constructor(private fb:FormBuilder) {
+
+    this.listData = [];
+
     this.userForm = this.fb.group({
       name:['', Validators.required],
       address:['', Validators.required],
       concactNo:['', Validators.required],
       gender:['', Validators.required],
     })
+  }
+
+  public addItem() : void{
+    this.listData.push(this.userForm.value);
+    this.userForm.reset();
+
+  }
+
+  removeItem(element: any){
+    this.listData.forEach((value: any,index: any) => {
+      if (value == element)
+        this.listData.splice(index,1);
+    });
+  }
+
+  reset(){
+    this.userForm.reset();
   }
 
   ngOnInit(): void {
